42e76ee110d53222d6cbe809734f5cdfdd889c1b
Ilya Ivanov <static.ila@gmail.com>
2024-04-17 21:41:29 +0300

    Add deploy script

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = (env, argv) => {
     return {
         entry: "./src/index.ts",
         output: {
-            path: path.resolve(__dirname, "dist"),
+            path: path.resolve(__dirname, "build"),
             filename: "app.[chunkhash].js",
         },
         devtool: isProd ? undefined : "inline-source-map",
